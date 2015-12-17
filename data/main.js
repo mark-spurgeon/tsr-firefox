222b0833ddbb33be48dbd9b70889b39ebe6ce027
Mark Spurgeon <theduck.dev@gmail.com>
2015-12-17 15:12:54 +0100

    Initial Commit

---
+++ data/main.js
@@ -0,0 +1,43 @@
+
+
+
+function openTab(url){self.port.emit('open-link', url.toString())}
+
+window.addEventListener('click', function(event) {
+  var t = event.target;
+   if (t.id == 'settings') {
+     self.port.emit('open-link', '#settings');
+   }
+   if (t.id == 'back') {
+     self.port.emit('open-link', '#start');
+   }
+   if (t.id == 'reload') {
+     self.port.emit('open-link', '#reload');
+   }
+   if (t.id == 'sp-edit') {
+     self.port.emit('open-link', '#sp-edit');
+   }
+   if (t.id == 'sp-add') {
+     self.port.emit('open-link', '#sp-add');
+   }
+   if (t.id == 'report') {
+     self.port.emit('open-link', '#report');
+   }
+   if (t.id == 'review') {
+     self.port.emit('open-link', '#review');
+   }
+
+  }, false)
+
+window.addEventListener('submit', function(event) {
+    var t = document.getElementById('text');
+    if ((t.value.indexOf('http://that.startpage.rocks/') === 0) || (t.value.indexOf('that.startpage.rocks/') === 0)){
+      id = t.value.split('/');
+      id = id[id.length - 1];
+      var new_url = 'http://that.startpage.rocks/firefox_addon/'+id;
+      self.port.emit('set-url',new_url);
+    } else {
+      /*do something */
+    }
+
+    }, false)
---
+++ index.js
@@ -0,0 +1,91 @@
+var { ToggleButton } = require('sdk/ui/button/toggle');
+var panels = require("sdk/panel");
+var self = require("sdk/self");
+var prefs = require("sdk/simple-prefs");
+var tabs = require("sdk/tabs");
+
+function getToggleButtonIcon(enabled) {
+      return {
+        "16": (enabled ? './icon-16-enabled.png' : './icon-16.png'),
+        "32": (enabled ? './icon-32-enabled.png' : './icon-32.png'),
+        "64": (enabled ? './icon-64-enabled.png' : './icon-64.png')
+      }
+    }
+
+var start_url = '';
+
+if ((prefs.prefs.spURL.indexOf('http://that.startpage.rocks/firefox_addon/') === 0)) {
+      start_url =prefs.prefs.spURL;
+    } else {
+      start_url = 'start.html';
+}
+
+
+var button = ToggleButton({
+  id: "my-button",
+  label: "That Startpage Rocks - Apps",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onChange: handleChange
+});
+
+
+var panel = panels.Panel({
+  contentURL: self.data.url(start_url),
+  onHide: handleHide,
+  contentScriptFile: self.data.url("main.js"),
+  width:300,
+  height:500
+});
+
+panel.port.on("open-link", function(url) {
+  if (url == '#settings') {
+    panel.contentURL = self.data.url('settings.html')
+  }
+  if (url == '#start') {
+    panel.contentURL = self.data.url(prefs.prefs.spURL);
+  }
+  if (url == '#reload') {
+    panel.contentURL = self.data.url('start.html');
+  }
+  if (url == '#sp-edit') {
+    tabs.open('http://that.startpage.rocks/edit');
+    panel.hide();
+  }
+  if (url == '#sp-add') {
+    tabs.open('http://that.startpage.rocks/edit');
+    panel.hide();
+  }
+  if (url == '#report') {
+    tabs.open('https://github.com/the-duck/tsr-firefox');
+    panel.hide();
+  }
+  if (url == '#review') {
+    tabs.open('http://that.startpage.rocks/edit');
+    panel.hide();
+  }
+});
+panel.port.on("set-url", function(url) {
+  if (url!='') {
+    panel.contentURL = url;
+    prefs.prefs.spURL = url;
+  }
+});
+
+
+function handleChange(state) {
+  if (state.checked) {
+    button.icon = getToggleButtonIcon(state.checked);
+    panel.show({
+      position: button
+    });
+  }
+}
+
+function handleHide() {
+  button.state('window', {checked: false});
+    button.icon = getToggleButtonIcon(false);
+}
